9df6c0cf07d214ace02b33fb0a30e4dc9f0a4094
LeoUnglaub <leo@leo-unglaub.net>
2010-08-29 22:57:10 +0000

    - add all files to the svn server
    git-svn-id: http://svn.lu-hosting.net/random_module@4 8dbd720c-4ce1-4db0-80f3-da40dfda1c42

---
+++ trunk/system/modules/random_module/config/database.sql
@@ -0,0 +1,17 @@
+-- ********************************************************
+-- *                                                      *
+-- * IMPORTANT NOTE                                       *
+-- *                                                      *
+-- * Do not import this file manually but use the Contao  *
+-- * install tool to create and maintain database tables! *
+-- *                                                      *
+-- ********************************************************
+
+-- 
+-- Table `tl_module`
+-- 
+
+CREATE TABLE `tl_module` (
+  `rm_active_modules` blob NULL,
+  `rm_indexer` varchar(20) NOT NULL default '',
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
